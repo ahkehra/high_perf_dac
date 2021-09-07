597f254b50ff73cedf416a4807df05c8388e502f
akirasupr <vishal.rockstar7011@gmail.com>
2021-09-07 17:19:36 +0530

    module:install: improve description

--- common/install.sh
+++ common/install.sh
@@ -11,6 +11,7 @@ ui_print "- Hardware: $(getprop ro.hardware)"
 ui_print ""
 if [ "$(getprop ro.hardware 2>/dev/null)" == "qcom" ]; then
   ui_print "- Installing High Performance DAC, Qualcomm Detected"
+  sed -i '/description=null/s/.*/description=Enable High Performance DAC of Qualcomms WCD9xx DAC to maximize its power and get the best audio quality possible./' $MODPATH/module.prop
   ui_print ""
 else
   if [ ! -d /sys/kernel/sound_control ]; then
@@ -20,6 +21,7 @@ else
   else
   ui_print "- Installing Sound Boost, Kernel Supported"
   sed -i '/$MODDIR/high-perf-dac.sh/s/.*/#$MODDIR/high-perf-dac.sh/' $MODPATH/post-fs-data.sh
+  sed -i '/description=null/s/.*/description=Enable Sound Boost of Kernel./' $MODPATH/module.prop
   rm -rf $MODPATH/high-perf-dac.sh
   ui_print ""
   fi
