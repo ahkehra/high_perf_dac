25e4a3f8290ed5925361379f76b569ad0393b813
akirasupr <vishal.rockstar7011@gmail.com>
2021-09-07 15:22:32 +0530

    high-perf-dac: fix

--- high-perf-dac.sh
+++ high-perf-dac.sh
@@ -5,11 +5,11 @@ MOUNT=/data
 MODDIR=${0%/*}
 LOG=$MOUNT/high_perf_dac
 if [ ! -d /sys/kernel/sound_control ]; then
+    echo "Disabled Sound Boost Excecuted on $(date +"%d-%m-%Y %r" )" >> $LOG/akirasupr.log
+else
     echo "10 10" > /sys/kernel/sound_control/headphone_gain
     echo 5 > /sys/kernel/sound_control/mic_gain
     echo 5 > /sys/kernel/sound_control/earpiece_gain
     echo 5 > /sys/kernel/sound_control/speaker_gain
     echo "Enabled Sound Boost Excecuted on $(date +"%d-%m-%Y %r" )" >> $LOG/akirasupr.log
-else
-    echo "Disabled Sound Boost Excecuted on $(date +"%d-%m-%Y %r" )" >> $LOG/akirasupr.log
 fi
